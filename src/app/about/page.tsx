5f97956a3b409837409c9d48818bb3fe4a770253
jaibararia <jaibararia@gmail.com>
2025-07-18 18:38:36 +0530

    issue fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
     return(
         <>
         <h1>About Me</h1>;
-        <p>Hello! I'm Jai, a developer passionate about building web applications using modern frameworks like Next.js.</p>
+        <p>Hello! I&apos;m Jai, a developer passionate about building web applications using modern frameworks like Next.js.</p>
         </>
     );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
       <div>
         <h1>404 - Page Not Found</h1>
-        <p>Oops! The page you are looking for doesn't exist.</p>
+        <p>Oops! The page you are looking for doesn&apos;t exist.</p>
       </div>
     );
   }
